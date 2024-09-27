c5902277ec2556278928a84c0ee29a1823a2155d
javierfc16 <javierfc1611@gmail.com>
2024-09-27 11:34:57 -0400

    qwewqeqw

---
+++ src/main/java/com/usic/qr_fest/controller/CamaraQrController.java
@@ -0,0 +1,53 @@
+package com.usic.qr_fest.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.usic.qr_fest.model.Entity.Entrada;
+import com.usic.qr_fest.model.IService.IEntradaService;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Controller
+public class CamaraQrController {
+
+    @Autowired
+    private IEntradaService entradaService;
+
+    @GetMapping("/camara_qr")
+    public String vistaGenerarQr(@RequestParam("codigoEncriptado") String codigoEncriptado, Model model) throws UnsupportedEncodingException {
+
+        // Decodificar el código encriptado para manejar caracteres especiales
+        
+
+        String adjustedCodigoEncriptado = codigoEncriptado.replaceAll(" ", "+");
+
+        if (entradaService.findIdEntradaByIdentificador(adjustedCodigoEncriptado) != null) {
+
+            if (entradaService.findEstadoByIdentificador(adjustedCodigoEncriptado).equals("NO APROBADO")) {
+                Entrada entrada = entradaService.findOne(entradaService.findIdEntradaByIdentificador(adjustedCodigoEncriptado));    
+                entrada.setEstado("APROBADO");
+                entradaService.save(entrada);
+                model.addAttribute("variable", "ENTRADA APROBADA");
+                model.addAttribute("variable2", "1");
+
+            }else{
+                model.addAttribute("variable", "LA ENTRADA YA FUE APROBADA");
+                model.addAttribute("variable2", "2");
+            }
+        }else{
+            model.addAttribute("variable", "LA ENTRADA NO EXISTE, POSIBLE FALSIFICACION");
+            model.addAttribute("variable2", "3");
+        }
+        
+        return "/camara_qr";
+    }
+
+}
