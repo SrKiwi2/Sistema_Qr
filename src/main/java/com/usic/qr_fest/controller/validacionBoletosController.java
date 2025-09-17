c46c0ddbde3541ce8d097c26c6a78f3fb2a5a5a5
jhowillneytan <jhowillneytan@gmail.com>
2025-09-16 20:42:14 -0400

    todocompleto_kiwi

--- src/main/java/com/usic/qr_fest/controller/validacionBoletosController.java
+++ src/main/java/com/usic/qr_fest/controller/validacionBoletosController.java
@@ -10,7 +10,42 @@ import lombok.RequiredArgsConstructor;
 public class validacionBoletosController {
     
     @GetMapping(value = "/pre-venta-mara")
-    public String VistaAdministrador() {
-        return "boletos/mara";
+    public String marasantos() {
+        return "boletos/preventa/mara";
+    }
+
+    @GetMapping(value = "/pre-venta-joseca")
+    public String joseca() {
+        return "boletos/preventa/joseca";
+    }
+
+    @GetMapping(value = "/pre-venta-euphoria")
+    public String euphoria() {
+        return "boletos/preventa/euphoria";
+    }
+
+    @GetMapping(value = "/pre-venta-combo")
+    public String combo() {
+        return "boletos/preventa/combo";
+    }
+
+    @GetMapping(value = "/venta-mara")
+    public String marasantos2() {
+        return "boletos/venta/mara";
+    }
+
+    @GetMapping(value = "/venta-joseca")
+    public String joseca2() {
+        return "boletos/venta/joseca";
+    }
+
+    @GetMapping(value = "/venta-euphoria")
+    public String euphoria2() {
+        return "boletos/venta/euphoria";
+    }
+
+    @GetMapping(value = "/venta-combo")
+    public String combo2() {
+        return "boletos/venta/combo";
     }
 }
